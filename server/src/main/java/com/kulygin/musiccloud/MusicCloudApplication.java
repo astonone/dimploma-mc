45df74d59675adc256651bc64a60a258e144fa0f
vitynya <viktor.kuligyn@smartbics.com>
2019-03-24 17:06:39 +0300

    auto creating storages

--- server/src/main/java/com/kulygin/musiccloud/MusicCloudApplication.java
+++ server/src/main/java/com/kulygin/musiccloud/MusicCloudApplication.java
@@ -1,11 +1,13 @@
 package com.kulygin.musiccloud;
 
+import com.kulygin.musiccloud.config.ExternalConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicCloudApplication {
 	public static void main(String[] args) {
+		ExternalConfig.createUploadFolders();
 		SpringApplication.run(MusicCloudApplication.class, args);
 	}
 }
